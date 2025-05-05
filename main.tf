c676e6b4538032859f47e75e9e439aadce184814
masato-N918 <b152122y@gmail.com>
2025-05-05 14:16:09 +0900

    ss12

--- main.tf
+++ main.tf
@@ -8,6 +8,11 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "tf-lecture33-nakama"
+    key    = "tastylog-dev.tfstate"
+    region = "ap-northeast-1"
+  }
 }
 
 #---------------------------
